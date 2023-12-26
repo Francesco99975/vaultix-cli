5969de9c3679d35bcabd7e46f16cb09740b91bda
Francesco Michele Barranca <francescomich99@gmail.com>
2023-12-26 12:23:19 -0500

    first commit

---
+++ src/cli.rs
@@ -0,0 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(about = "Signup to Vaultix")]
+    Signup {
+        #[arg(short = 'p', long = "password")]
+        password: Option<String>,
+    },
+    #[command(about = "Login to Vaultix")]
+    Login {
+        #[arg(short = 'p', long = "password")]
+        password: Option<String>,
+    },
+    #[command(about = "List all stored encrypted credentials")]
+    List,
+    #[command(about = "Encrypt and store credential")]
+    Store {
+        service: Option<String>,
+        credentials: Option<Vec<String>>,
+    },
+    #[command(about = "Decrypt and display specified credential")]
+    Show { credential: Option<String> },
+}
+
+#[derive(Parser, Debug)]
+#[command(author = "Francesco Michele Barranca (kalairendev)", version = "1.0", about = "Vaultix Credentials Manager", long_about = None)]
+#[command(propagate_version = true)]
+struct Vaultix {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
